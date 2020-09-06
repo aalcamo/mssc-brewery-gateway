9b59dadafe10db8f7d054120c43328208668a475
aalcamo <aalcamo@gofurthercorp.com>
2020-09-05 20:27:31 -0400

    added spring security.

--- src/main/java/com/sfg/gateway/config/GoogleConfig.java
+++ src/main/java/com/sfg/gateway/config/GoogleConfig.java
@@ -3,29 +3,26 @@ package com.sfg.gateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("google")
-@Configuration
+/**
+ * Created by jt on 3/5/20.
+ */
+//@Profile("google")
+//@Configuration
 public class GoogleConfig {
 
     /**
-     * when I come into the gateway with a /googlesearch request, we
-     * will redirect to google.com.
-     *
+     * This is just an example of routing requests to Google from localhost. For demonstration only
      * @param builder
      * @return
      */
     @Bean
-    public RouteLocator googleConfig(RouteLocatorBuilder builder) {
-
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder){
         return builder.routes()
-                .route(r -> r.path("/googlesearch")
+                .route(r -> r.path("/googlesearch2")
+                        .filters(f -> f.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
                         .uri("https://google.com")
                         .id("google"))
                 .build();
-
     }
 }
-
